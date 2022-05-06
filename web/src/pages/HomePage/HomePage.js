7a60439d1589b7265b9cf8622fe590db095b1119
Prawee Wongsa <prawee@hotmail.com>
2022-05-06 18:11:40 +0700

    update home page with content only

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,20 +1,10 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <Link to={routes.about()}>About</Link>
-          </ul>
-        </nav>
-      </header>
-      <main>Home</main>
+      <>Home</>
     </>
   )
 }
