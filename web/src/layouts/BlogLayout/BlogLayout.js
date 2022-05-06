5834f5f5378d713d24f190ef4fd46136c02e055c
Prawee Wongsa <prawee@hotmail.com>
2022-05-06 18:25:06 +0700

    adding home link to blog layout

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -11,6 +11,9 @@ const BlogLayout = ({ children }) => {
         <nav>
           <ul>
             <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+            <li>
               <Link to={routes.about()}>About</Link>
             </li>
           </ul>
